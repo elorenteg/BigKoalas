0e8767b75e55b7989681b8584d341386e4b9397f
Marc Vila <marcvilagomez@gmail.com>
2018-10-17 17:54:04 +0200

    Add bottomlayout

--- android/app/src/main/java/android/bigiot/org/androidexampleconsumer/MainActivity.java
+++ android/app/src/main/java/android/bigiot/org/androidexampleconsumer/MainActivity.java
@@ -13,18 +13,41 @@ package android.bigiot.org.androidexampleconsumer;
 
 import android.bigiot.org.androidexampleconsumer.controller.BigIotController;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    return true;
+                case R.id.navigation_dashboard:
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         try {
             BigIotController.getInstance(this).accessOffering();
