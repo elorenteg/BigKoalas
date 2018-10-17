cb2595307dd4f09530d9c1f8f142fb0b7ca9b8d9
Marc Vila <marcvilagomez@gmail.com>
2018-10-17 20:00:35 +0200

    Show map in fragment

---
+++ android/app/src/main/java/android/bigiot/org/androidexampleconsumer/HomeFragment.java
@@ -0,0 +1,92 @@
+package android.bigiot.org.androidexampleconsumer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+public class HomeFragment extends Fragment {
+    public static final String TAG = HomeFragment.class.getSimpleName();
+    private View rootView;
+    private MapView mapView;
+
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        setUpElements();
+        setUpListeners();
+
+        Mapbox.getInstance(getActivity(), getString(R.string.access_token));
+        mapView = rootView.findViewById(R.id.mapView);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+    }
+
+
+    private void setUpElements() {
+
+    }
+
+    private void setUpListeners() {
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
