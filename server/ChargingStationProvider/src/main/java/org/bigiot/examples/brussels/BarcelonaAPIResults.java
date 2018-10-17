3b4c8f2ecd22eec14a7de7dfd9f6905e2bc701e0
elorenteg <ester.lorente@est.fib.upc.edu>
2018-10-18 00:04:42 +0200

    Add Brussels offering

--- server/ChargingStationProvider/src/main/java/org/bigiot/examples/brussels/BarcelonaAPIResults.java
+++ server/ChargingStationProvider/src/main/java/org/bigiot/examples/brussels/BarcelonaAPIResults.java
@@ -4,157 +4,289 @@ import java.util.List;
 
 public class BarcelonaAPIResults {
 
-    private Result result;
+    private String type;
+    private int totalFeatures;
+    private List<Feature> features;
 
-    public Result getResultParent() {
-        return result;
-    }
-    public void setResultParent(Result result) {
-        this.result = result;
-    }
-    public String toString() { return "ResponseData[" + result.toString() + "]"; }
-
-    static class Result {
-        private List<Info> chargepoint;
-
-        public List<Info> getChargepoint() {
-            return chargepoint;
-        }
-        public void setChargepoint(List<Info> chargepoint) {
-            this.chargepoint = chargepoint;
-        }
-        public String toString() { return "ResponseData[" + chargepoint.toString() + "]"; }
+    public String  toString() {
+        return totalFeatures + "";
     }
 
-    static class Info {
-        private String ParkingID;
-        private String ParkingName;
-        private String Address;
-        private Float Lat;
-        private Float Lng;
-        private Object ParkingLevel;
-        private Object Place;
-        private String Vehicle;
-        private String ChargeBoxID;
-        private String Schuko;
-        private Object Menneke;
-        private Object Status;
-        private String Created;
-        private String Updated;
+    public String getType() {
+        return type;
+    }
 
-        public String getParkingID() {
-            return ParkingID;
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getTotalFeatures() {
+        return totalFeatures;
+    }
+
+    public void setTotalFeatures(int totalFeatures) {
+        this.totalFeatures = totalFeatures;
+    }
+
+    public List<Feature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<Feature> features) {
+        this.features = features;
+    }
+
+    static class Properties{
+
+        private int id;
+        private int number;
+        private String owner;
+        private String type;
+        private String type_fr;
+        private String type_nl;
+        private String payment;
+        private String charging;
+        private String charging_fr;
+        private String charging_nl;
+        private String plug;
+        private String plug_nl;
+        private String plug_fr;
+        private String road_fr;
+        private String road_nl;
+        private String housenr;
+        private String pccp;
+        private String mu_fr;
+        private String mu_nl;
+        private List<Float> bbox;
+
+        public int getId() {
+            return id;
         }
 
-        public void setParkingID(String parkingID) {
-            ParkingID = parkingID;
+        public void setId(int id) {
+            this.id = id;
         }
 
-        public String getParkingName() {
-            return ParkingName;
+        public int getNumber() {
+            return number;
         }
 
-        public void setParkingName(String parkingName) {
-            ParkingName = parkingName;
+        public void setNumber(int number) {
+            this.number = number;
         }
 
-        public String getAddress() {
-            return Address;
+        public String getOwner() {
+            return owner;
         }
 
-        public void setAddress(String address) {
-            Address = address;
+        public void setOwner(String owner) {
+            this.owner = owner;
         }
 
-        public Float getLat() {
-            return Lat;
+        public String getType() {
+            return type;
         }
 
-        public void setLat(Float lat) {
-            Lat = lat;
+        public void setType(String type) {
+            this.type = type;
         }
 
-        public Float getLng() {
-            return Lng;
+        public String getType_fr() {
+            return type_fr;
         }
 
-        public void setLng(Float lng) {
-            Lng = lng;
+        public void setType_fr(String type_fr) {
+            this.type_fr = type_fr;
         }
 
-        public Object getParkingLevel() {
-            return ParkingLevel;
+        public String getType_nl() {
+            return type_nl;
         }
 
-        public void setParkingLevel(Object parkingLevel) {
-            ParkingLevel = parkingLevel;
+        public void setType_nl(String type_nl) {
+            this.type_nl = type_nl;
         }
 
-        public Object getPlace() {
-            return Place;
+        public String getPayment() {
+            return payment;
         }
 
-        public void setPlace(Object place) {
-            Place = place;
+        public void setPayment(String payment) {
+            this.payment = payment;
         }
 
-        public String getVehicle() {
-            return Vehicle;
+        public String getCharging() {
+            return charging;
         }
 
-        public void setVehicle(String vehicle) {
-            Vehicle = vehicle;
+        public void setCharging(String charging) {
+            this.charging = charging;
         }
 
-        public String getChargeBoxID() {
-            return ChargeBoxID;
+        public String getCharging_fr() {
+            return charging_fr;
         }
 
-        public void setChargeBoxID(String chargeBoxID) {
-            ChargeBoxID = chargeBoxID;
+        public void setCharging_fr(String charging_fr) {
+            this.charging_fr = charging_fr;
         }
 
-        public String getSchuko() {
-            return Schuko;
+        public String getCharging_nl() {
+            return charging_nl;
         }
 
-        public void setSchuko(String schuko) {
-            Schuko = schuko;
+        public void setCharging_nl(String charging_nl) {
+            this.charging_nl = charging_nl;
         }
 
-        public Object getMenneke() {
-            return Menneke;
+        public String getPlug() {
+            return plug;
         }
 
-        public void setMenneke(Object menneke) {
-            Menneke = menneke;
+        public void setPlug(String plug) {
+            this.plug = plug;
         }
 
-        public Object getStatus() {
-            return Status;
+        public String getPlug_nl() {
+            return plug_nl;
         }
 
-        public void setStatus(Object status) {
-            Status = status;
+        public void setPlug_nl(String plug_nl) {
+            this.plug_nl = plug_nl;
         }
 
-        public String getCreated() {
-            return Created;
+        public String getPlug_fr() {
+            return plug_fr;
         }
 
-        public void setCreated(String created) {
-            Created = created;
+        public void setPlug_fr(String plug_fr) {
+            this.plug_fr = plug_fr;
         }
 
-        public String getUpdated() {
-            return Updated;
+        public String getRoad_fr() {
+            return road_fr;
         }
 
-        public void setUpdated(String updated) {
-            Updated = updated;
+        public void setRoad_fr(String road_fr) {
+            this.road_fr = road_fr;
         }
 
-        public String toString() { return getParkingID(); }
+        public String getRoad_nl() {
+            return road_nl;
+        }
+
+        public void setRoad_nl(String road_nl) {
+            this.road_nl = road_nl;
+        }
+
+        public String getHousenr() {
+            return housenr;
+        }
+
+        public void setHousenr(String housenr) {
+            this.housenr = housenr;
+        }
+
+        public String getPccp() {
+            return pccp;
+        }
+
+        public void setPccp(String pccp) {
+            this.pccp = pccp;
+        }
+
+        public String getMu_fr() {
+            return mu_fr;
+        }
+
+        public void setMu_fr(String mu_fr) {
+            this.mu_fr = mu_fr;
+        }
+
+        public String getMu_nl() {
+            return mu_nl;
+        }
+
+        public void setMu_nl(String mu_nl) {
+            this.mu_nl = mu_nl;
+        }
+
+        public List<Float> getBbox() {
+            return bbox;
+        }
+
+        public void setBbox(List<Float> bbox) {
+            this.bbox = bbox;
+        }
+    }
+
+    static class Geometry{
+        private String type;
+        private List<Float> coordinates;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public List<Float> getCoordinates() {
+            return coordinates;
+        }
+
+        public void setCoordinates(List<Float> coordinates) {
+            this.coordinates = coordinates;
+        }
+    }
+
+    static class Feature {
+        private String type;
+        private String id;
+        private Geometry geometry;
+        private String geometry_name;
+        private Properties properties;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public Geometry getGeometry() {
+            return geometry;
+        }
+
+        public void setGeometry(Geometry geometry) {
+            this.geometry = geometry;
+        }
+
+        public String getGeometry_name() {
+            return geometry_name;
+        }
+
+        public void setGeometry_name(String geometry_name) {
+            this.geometry_name = geometry_name;
+        }
+
+        public Properties getProperties() {
+            return properties;
+        }
+
+        public void setProperties(Properties properties) {
+            this.properties = properties;
+        }
     }
 
 }
